5ab48e6097bb200ca7f33c4b9bdb2b03467066df
Flavio Fernandes <flavio@flaviof.com>
2016-07-29 10:48:25 -0500

    tweak tenant subnet for l3_basic setup
    use 192.168.1.x and 192.168.2.y instead of 1.0.0.x and 2.0.0.y

--- scripts/tutorial/l3_basic/setup.sh
+++ scripts/tutorial/l3_basic/setup.sh
@@ -15,8 +15,8 @@ sudo ovn-nbctl lsp-add ls1 ls1-port1
 sudo ovn-nbctl lsp-add ls2 ls2-port1
 
 # Set a MAC address for each of the two logical ports.
-sudo ovn-nbctl lsp-set-addresses ls1-port1 "00:00:00:00:00:01 1.0.0.1"
-sudo ovn-nbctl lsp-set-addresses ls2-port1 "00:00:00:00:00:02 2.0.0.1"
+sudo ovn-nbctl lsp-set-addresses ls1-port1 "00:00:00:00:00:01 192.168.1.10"
+sudo ovn-nbctl lsp-set-addresses ls2-port1 "00:00:00:00:00:02 192.168.2.10"
 
 # Set up port security for the two logical ports.  This ensures that
 # the logical port mac address we have configured is the only allowed
@@ -26,25 +26,25 @@ sudo ovn-nbctl lsp-set-port-security ls2-port1 00:00:00:00:00:02
 
 # Create ports on the local OVS bridge, br-int.
 ## create-ns-port.sh <NODE> <OVN_LSP> <MAC> <IP/MASK> <IP_GW>
-/vagrant/scripts/create-ns-port.sh compute1 ls1-port1 00:00:00:00:00:01 1.0.0.1/24 1.0.0.2
-/vagrant/scripts/create-ns-port.sh compute2 ls2-port1 00:00:00:00:00:02 2.0.0.1/24 2.0.0.2
+/vagrant/scripts/create-ns-port.sh compute1 ls1-port1 00:00:00:00:00:01 192.168.1.10/24 192.168.1.1
+/vagrant/scripts/create-ns-port.sh compute2 ls2-port1 00:00:00:00:00:02 192.168.2.10/24 192.168.2.1
 
 #---
 
-# Add a logical router, so 1.0.0.1 can reach 2.0.0.1
+# Add a logical router, so 192.168.1.10 can reach 192.168.2.10
 
 sudo ovn-nbctl lr-add lr0
 
-sudo ovn-nbctl lrp-add lr0 lr0p1 00:00:00:01:00:01 1.0.0.2/24
+sudo ovn-nbctl lrp-add lr0 lr0p1 00:00:00:01:00:01 192.168.1.1/24
 sudo ovn-nbctl -- lsp-add ls1 lr0p1-attachment \
                -- set Logical_Switch_Port lr0p1-attachment \
                   type=router \
                   options:router-port=lr0p1 \
-                  addresses='"00:00:00:01:00:01 1.0.0.2"'
+                  addresses='"00:00:00:01:00:01 192.168.1.1"'
 
-sudo ovn-nbctl lrp-add lr0 lr0p2 00:00:00:01:00:02 2.0.0.2/24
+sudo ovn-nbctl lrp-add lr0 lr0p2 00:00:00:01:00:02 192.168.2.1/24
 sudo ovn-nbctl -- lsp-add ls2 lr0p2-attachment \
                -- set Logical_Switch_Port lr0p2-attachment \
                   type=router \
                   options:router-port=lr0p2 \
-                  addresses='"00:00:00:01:00:02 2.0.0.2"'
+                  addresses='"00:00:00:01:00:02 192.168.2.1"'
